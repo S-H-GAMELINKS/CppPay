47c90fcff25d7c14a0ef722dee007a67c7110533
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-19 22:43:30 +0900

    using Header.vue for nav-bar tag #9

--- assets/src/index.js
+++ assets/src/index.js
@@ -3,10 +3,15 @@ import Vue from 'vue';
 import * as Bootstrap from 'bootstrap-umi';
 import 'bootstrap-umi/dist/css/bootstrap.css';
 
+import Header from '../components/layouts/Header.vue';
+
 Vue.use(Bootstrap);
 
 const app = new Vue({
     el: '#app',
+    components: {
+        'nav-bar': Header,
+    },
     data: function() {
         return {
             message: "Hello World!"
