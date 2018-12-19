e7c2e7f21d7a2d072230a16e5bad5470409c9a64
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-19 22:46:54 +0900

    fix path

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/index.js', 
     output: { 
       filename: 'index.js',
-      path: `${__dirname}` 
+      path: `${__dirname}/` 
     },
     module: {
         rules: [
