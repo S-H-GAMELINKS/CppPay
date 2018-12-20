29a2644eeb42ba545c4133af3199fd2a5fb83258
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-20 16:18:43 +0900

    add session for Firebase #14

--- assets/store/store.js
+++ assets/store/store.js
@@ -1,9 +1,44 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import * as FireBase from 'firebase';
+
 Vue.use(Vuex);
 
+const firebase = FireBase.initializeApp({
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DB_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGEING_SENDER_ID
+});
+
+const database = firebase.database();
+
 const store = new Vuex.Store({
+    state: {
+        firebase: firebase,
+        database: database,
+        id: "",
+        session: false
+    },
+    mutations: {
+        loginBlog(state, id) {
+            state.id = String(id);
+        },
+        getSession(state, bool) {
+            state.session = bool;
+        },
+        deleteSession(state, bool) {
+            state.session = bool;
+        }
+    },
+    plugins: [ createPersistedState({
+        key: 'loginSession',
+        paths: ['session'],
+        storage: window.sessionStorage,
+    })]
 });
 
 export default store;
